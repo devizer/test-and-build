8b1175510ff040119316912d516219952e5e5909
devizer <devizer@users.github.com>
2019-11-15 00:39:25 +0200

    1st i386

--- image-builder.ps1
+++ image-builder.ps1
@@ -78,7 +78,7 @@ qemu-system-i386 -smp $($startParams.Cores) -m $($startParams.Mem) -M q35 \
     -initrd initrd.img \
     -kernel vmlinuz -append "root=/dev/sda1 console=ttyS0" \
     -drive file=$($fileName),cache=unsafe,if=none \
-    -drive file=ephemeral.qcow2,id=ephemeral,cache=unsafe,if=none -device scsi-hd,drive=ephemeral \
+    -drive file=ephemeral.qcow2,id=ephemeral,cache=unsafe,if=none \
     -netdev user,hostfwd=tcp::$($startParams.Port)-:22,id=unet -device rtl8139,netdev=unet \
     -net user \
     -nographic
