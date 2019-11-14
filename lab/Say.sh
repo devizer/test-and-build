1fb02c41bf3b6220706b8a4ca8639d7d8b3b4e8c
devizer <devizer@users.github.com>
2019-11-14 08:44:48 +0200

    +Say

--- lab/Say.sh
+++ lab/Say.sh
@@ -1,8 +1,7 @@
-
+#!/usr/bin/env bash
 
     function print_header() {
       # if [[ -e /tmp/
-      # user=
       SYSTEM="${SYSTEM:-$(uname -s)}"
       if [[ ${SYSTEM} != Darwin ]]; then
           uptime=$(</proc/uptime);                  # 42645.93 240538.58
@@ -14,7 +13,13 @@
       LightGreen='\033[1;32m'; Yellow='\033[1;33m'; RED='\033[0;31m'; NC='\033[0m'; LightGray='\033[1;2m';
       printf "${LightGray}${uptime:-}${NC} ${LightGreen}$1${NC} ${Yellow}$2${NC}\n"; 
     }
-    counter=0; 
-    function Say() { counter=$((counter+1)); print_header " #$counter" "$1"; }; 
-    Say "" >/dev/null;
-    counter=0;
+    function SayIt() { 
+      user="${LOGNAME:-$(whoami)}"
+      file="/tmp/.${user}-said-counter"
+      if [[ -e "$file" ]]; then counter=$(< "$file"); else counter=1; fi
+      print_header " $counter" "$1";
+      counter=$((counter+1));
+      echo $counter > "$file"
+    }; 
+
+SayIt "$@"
--- lab/config-system.sh
+++ lab/config-system.sh
@@ -11,7 +11,9 @@ export TZ=Europe/London
 ' | sudo tee ~/.bashrc > /dev/null
 sudo timedatectl set-timezone UTC
 
-cat /tmp/build/Say.sh >> ~/.bashrc
-cat /tmp/build/Say.sh >> ~/.profile
+# cat /tmp/build/Say.sh >> ~/.bashrc
+# cat /tmp/build/Say.sh >> ~/.profile
+sudo cp /tmp/build/Say.sh /usr/local/bin/Say
+chmod +x /usr/local/bin/Say
 
-apt-get install -qq libunwind8 -y 
+# apt-get install -qq libunwind8 -y 
