f614c26c64fec6d0f6834f352a53993b1b2e0934
devizer <devizer@users.github.com>
2019-11-16 04:41:03 +0200

    duplicated mono key

--- lab/install-MONO.sh
+++ lab/install-MONO.sh
@@ -2,7 +2,7 @@
 if [[ "$(command -v mono)" == "" ]]; then
   apt-update 
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6A19B38D3D831EF
-  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+#  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
   source /etc/os-release
   def="deb https://download.mono-project.com/repo/$ID stable-$(lsb_release -s -c) main"
   if [[ "$ID" == "raspbian" ]]; then def="deb https://download.mono-project.com/repo/debian stable-raspbian$(lsb_release -cs) main"; fi
