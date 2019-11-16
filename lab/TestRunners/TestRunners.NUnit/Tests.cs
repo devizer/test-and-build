b95feb25d7b153416214576a087f60d3801bc401
devizer <devizer@users.github.com>
2019-11-16 03:21:52 +0200

    added unit-test sources

---
+++ lab/TestRunners/TestRunners.NUnit/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestRunners.NUnit
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
