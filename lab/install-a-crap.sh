d0ef2c04b80eb1d56dabfb627f48bad11b431ecc
devizer <devizer@users.github.com>
2019-11-16 06:49:15 +0200

    clean up

--- lab/install-a-crap.sh
+++ lab/install-a-crap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 lazy-apt-update
-apt-get -y install sudo subversion git p7zip-full wget htop iotop curl ca-certificates sudo \
+apt-get -y install sudo subversion git p7zip-full wget htop ncdu iotop curl ca-certificates sudo \
    subversion git p7zip-full wget htop iotop curl ca-certificates \
    build-essential autoconf autoconf pkg-config libssl-dev \
    zlib1g zlib1g-dev make pv libncurses5-dev libncurses5 libncursesw5-dev libncursesw5 gettext \
