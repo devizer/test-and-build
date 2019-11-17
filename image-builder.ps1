9fa3bf524acbf4a21a570b8d63961939d3f8de79
devizer <devizer@users.github.com>
2019-11-17 21:04:56 +0200

    Log

--- image-builder.ps1
+++ image-builder.ps1
@@ -280,7 +280,7 @@ function Build
     $Is_Requested_Local_Redis = Is-Requested-Specific-Feature("local-redis");
 
     $featuresToInstall = $FeatureFilters | % { if (Is-Requested-Specific-Feature $_) { $_ } };
-    $featuresToSkip = $FeatureFilters | % { if (-not Is-Requested-Specific-Feature $_) { $_ } };
+    $featuresToSkip = $FeatureFilters | % { if (-not (Is-Requested-Specific-Feature $_)) { $_ } };
     Say "To INSTALL: [$featuresToInstall], To Skip: [$featuresToSkip]"  
     
     $key = $definition.key
