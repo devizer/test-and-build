c81f5999fe1e9aaf52bbae5e1f1bc1f310c6cda9
devizer <devizer@users.github.com>
2019-11-17 07:47:45 +0200

    escaping for quotes

--- lab/install-NODE.sh
+++ lab/install-NODE.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-# should be run as USER
+# should be run as ROOT
+
+export NVM_DIR="/opt/nvm"
+Say "Installing nvm to $NVM_DIR" 
+mkdir -p "$NVM_DIR"
 script=https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+chown user:user -R "$NVM_DIR" 
+[[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 is_386="$(uname -m | grep -E 'i.86' 2>/dev/null)"
@@ -14,18 +18,19 @@ if [[ "$ARCH" == i386 ]]; then
     apt clean -qq
 fi
 
-echo '
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-' >> ~/.bashrc
+cat > /tmp/nvm-env << EOL
+NVM_DIR='$NVM_DIR'
+export NVM_DIR 
+[ -s "\$NVM_DIR/nvm.sh" ] && \. "\$NVM_DIR/nvm.sh"  # This loads nvm
+EOL
 
-echo '
-export NVM_DIR="/home/user/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-' | sudo tee -a /root/.bashrc >/dev/null
+for file in "/etc/profile.d/nvm" "~/.profile" "~/.bashrc" "/home/user/.profile" "/home/user/.bashrc"; do
+    cat /tmp/nvm-env >> "$file"
+    chmod +x "$file"
+    if [[ "$file" == "/home/user"* ]]; then chown user:user "$file"; fi
+done
 
 Say "Installing NodeJS LTS"
-
 
 if [[ -n "$TRAVIS" ]]; then
 # without optimization it is also slow
@@ -46,7 +51,8 @@ Say "Default NodeJS version: $(nvm current)"
 # nvm use node
 # Say "New NodeJS version: $(nvm current)"
 Say "Upgrading NPM & NPX, installing YARN"
-time npm install yarn npm npx npm-check-updates --global
+# time npm install yarn npm npx npm-check-updates --global
+time npm install yarn --global
 time yarn config set network-timeout 600000 -g
 
 
