4e3f0f5077be2ef190e4c845a97fdabe26d34f5b
devizer <devizer@users.github.com>
2019-11-18 13:29:05 +0200

    parallel build

--- deploy/parallel-rebuild-all-debians.sh
+++ deploy/parallel-rebuild-all-debians.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-rm /transient-builds/test-and-build/*
+rm -rf /transient-builds/test-and-build/*
 cd ~/build/devizer/test-and-build
 git pull
 
@@ -8,9 +8,9 @@ mkdir -p $logs
 (pwsh -c ./image-builder.ps1 -Images i386  -MaxVmCores 3 -FinalSize 42G 2>&1 | tee $logs/i386.log  ) &
 job1=$!
 (pwsh -c ./image-builder.ps1 -Images arm   -MaxVmCores 2 -FinalSize 42G 2>&1 | tee $logs/arm.log   ) &
-job2=$1
+job2=$!
 (pwsh -c ./image-builder.ps1 -Images arm64 -MaxVmCores 2 -FinalSize 42G 2>&1 | tee $logs/arm64.log ) &
-$job3
+$job3=$!
 
 echo WAITING 3 JOBS: $(date)
 wait $job1
