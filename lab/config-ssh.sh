9b53282cce8f48ca41f3439232406190fe1e2047
devizer <devizer@users.github.com>
2019-11-19 06:03:08 +0200

    fix error in [cd /tmp/build; bash config-ssh.sh arm]

--- lab/config-ssh.sh
+++ lab/config-ssh.sh
@@ -8,12 +8,30 @@ for f in "Say" "try-and-retry" "smart-apt-install" "lazy-apt-update"; do
     sudo cp permanent-scripts/${f}.sh /usr/local/bin/${f}
     chmod +x /usr/local/bin/${f}
 done
+function config_loc() {
+    # echo "Content of /etc/default/locale:"; cat /etc/default/locale
+    # echo "-----------------"
 
+    # Say "Configure locales"
+    export DEBIAN_FRONTEND=noninteractive
+    echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+    echo "es_ES.UTF-8 UTF-8" >> /etc/locale.gen
+    echo "LANG=en_US.UTF-8" > /etc/locale.conf
+    echo '
+LC_ALL="en_US.UTF-8"
+LANG="en_US.UTF-8"
+'   | sudo tee /etc/default/locale > /dev/null
+    # locale-gen "en_US.UTF-8" "en_GB.UTF-8" "es_ES.UTF-8 UTF-8"
+    dpkg-reconfigure locales
+
+}
+
+config_loc
 echo "Say command: $(command -v Say)"
 
-sshId=$(pgrep -f "sshd -D")
-Say "Configure ssh environment and restarting ssh server (id is $sshId)"
-Say "Restarting ssh server"
+
+Say "Configure ssh environment"
 sed -i 's/#PermitUserEnvironment no/PermitUserEnvironment yes/g' /etc/ssh/sshd_config
 sed -i 's/AcceptEnv LANG LC_\*//g' /etc/ssh/sshd_config
 sed -i 's/#Compression delayed/Compression no/g' /etc/ssh/sshd_config
@@ -37,26 +55,7 @@ cat /etc/ssh/sshd_config
 
 if [[ false ]]; then 
     # TODO: extract it as a destructive separated command
-    sshId="$(pgrep -f "sshd -D")"; Say "Restarting SSH Server (id is $sshId)"; sudo kill -SIGHUP "$(pgrep -f "sshd -D")"; Say "Restarted SSH Server";
+    # sshId="$(pgrep -f "sshd -D")"; Say "Restarting SSH Server (id is $sshId)"; sudo kill -SIGHUP "$(pgrep -f "sshd -D")"; Say "Restarted SSH Server";
+    x=42;
 fi
 
-function config_loc() {
-# echo "Content of /etc/default/locale:"; cat /etc/default/locale
-# echo "-----------------"
-
-Say "Configure locales"
-    export DEBIAN_FRONTEND=noninteractive
-    echo "LC_ALL=en_US.UTF-8" >> /etc/environment
-    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-    echo "es_ES.UTF-8 UTF-8" >> /etc/locale.gen
-    echo "LANG=en_US.UTF-8" > /etc/locale.conf
-    echo '
-LC_ALL="en_US.UTF-8"
-LANG="en_US.UTF-8"
-'   | sudo tee /etc/default/locale > /dev/null
-    # locale-gen "en_US.UTF-8" "en_GB.UTF-8" "es_ES.UTF-8 UTF-8"
-    dpkg-reconfigure locales
-
-}
-
-config_loc
