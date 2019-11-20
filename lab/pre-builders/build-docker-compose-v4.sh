40ed97686543af437b40d5f09195a3dac6e3808e
devizer <devizer@users.github.com>
2019-11-20 16:28:22 +0200

    build-docker-compose-v4.sh

--- lab/pre-builders/build-docker-compose-v4.sh
+++ lab/pre-builders/build-docker-compose-v4.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -e
-DOCKER_COMPOSE_VER=1.24.1
+
+function build_docker_compose() {
+    echo "BUILDING docker-compose $DOCKER_COMPOSE_VER for $(uname -s)-$(uname -m)" 
+    git checkout $DOCKER_COMPOSE_VER 
+    time ./script/build/linux
+    # pattern: dist/1.24.1/docker-compose-Linux-armv7l-1.24.1
+    "./dist/docker-compose-$(uname -s)-$(uname -m)" version && (
+        artifact="~/docker-compose-artifact/$DOCKER_COMPOSE_VER/docker-compose-$(uname -s)-$(uname -m)-$DOCKER_COMPOSE_VER"
+        mkdir -p $(dirname $artifact) 
+        sudo cp -f "./dist/docker-compose-$(uname -s)-$(uname -m)" $artifact
+    ) || echo "Error building docker-compose $DOCKER_COMPOSE_VER"
+}
+
 lazy-apt-update
 apt install python-pip git -y
 systemctl start docker || true
@@ -9,11 +21,17 @@ command -v docker || (echo "Error. Install Docker first for this script"; exit 1
 work=/compose
 mkdir -p $work
 cd $work
-git clone https://github.com/docker/compose
-cd compose
+git clone https://github.com/docker/compose || (cd compose; git checkout master; git pull)
+cd $work/compose
 lastThreeVersions=$(git tag --sort=-v:refname | egrep -o "^([0-9]{1,}\.)+[0-9]{1,}$" | head -3)
-echo $lastThreeVersions
-git checkout $DOCKER_COMPOSE_VER
+echo "Last three non-rc versions: $lastThreeVersions"
+for DOCKER_COMPOSE_VER in $lastThreeVersions; do
+    echo "next: $DOCKER_COMPOSE_VER"
+    # build_docker_compose
+    skip=skip;
+done
+ 
 
-time ./script/build/linux
-"./dist/docker-compose--$(uname -s)-$(uname -m)" version
+DOCKER_COMPOSE_VER=1.24.1
+build_docker_compose
+
