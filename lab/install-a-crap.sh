7f95d6de8fe78d64e5c311385f06fd293f87f0cd
devizer <devizer@users.github.com>
2019-11-21 00:59:42 +0200

    smart-apt-install for build essentials

--- lab/install-a-crap.sh
+++ lab/install-a-crap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 lazy-apt-update
-apt-get -y install sudo subversion git p7zip-full wget htop ncdu iotop curl ca-certificates sudo \
+smart-apt-install sudo subversion git p7zip-full wget htop ncdu iotop curl ca-certificates sudo \
    subversion git p7zip-full wget htop iotop curl ca-certificates \
    build-essential autoconf autoconf pkg-config libssl-dev \
    zlib1g zlib1g-dev make pv libncurses5-dev libncurses5 libncursesw5-dev libncursesw5 gettext \
@@ -8,5 +8,5 @@ apt-get -y install sudo subversion git p7zip-full wget htop ncdu iotop curl ca-c
    cmake \
    ffmpeg imagemagick
 
-apt clean
+
 
