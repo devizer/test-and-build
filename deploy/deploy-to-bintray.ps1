8a5c8dc35ed5623272dd85ee716452df0ffb8876
devizer <devizer@users.github.com>
2019-11-22 22:57:14 +0200

    deploy-to-bintray.ps1 again

--- deploy/deploy-to-bintray.ps1
+++ deploy/deploy-to-bintray.ps1
@@ -1,4 +1,12 @@
 #!/usr/bin/env pwsh
+param(
+    [string] $from
+)
+
+$Working_Folder="/tmp/debian-to-bintray"
+& mkdir -p $Working_Folder
+
+
 
 pushd ../build
 & pwsh ./inject-git-info.ps1
@@ -6,3 +14,5 @@ popd
 
 $version=(& cat ../bintray.json | jq -r ".version.name") | Out-String 
 Write-Host "To Publish: $version"
+
+& cp ../bintray.json $Working_Folder
