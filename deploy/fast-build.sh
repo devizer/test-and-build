78c5027cd1f08f6c518d7bb8dfa513d0e336732c
devizer <devizer@users.github.com>
2019-11-23 15:52:20 +0200

    removed .git folder from artefact

--- deploy/fast-build.sh
+++ deploy/fast-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd ~/build/devizer/test-and-build; git pull; 
 # git pull; pwsh -c ./image-builder.ps1 -Images AMD64 -Skip None -FinalSize 0 -MaxVmCores 2
-git pull; pwsh -c ./image-builder.ps1 -Images AMD64 -Only mono -FinalSize 0 -MaxVmCores 2
+git pull; pwsh -c ./image-builder.ps1 -Images AMD64 -Only nothing -FinalSize 0 -MaxVmCores 2
 
 
