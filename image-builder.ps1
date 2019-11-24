d4bfefcf182c35160d0245a253616d9262e96b6f
devizer <devizer@users.github.com>
2019-11-24 15:55:13 +0200

    for bintray the splitting is by 49M, the stable

--- image-builder.ps1
+++ image-builder.ps1
@@ -592,7 +592,7 @@ function Build
     $finalArchivePath = "$(pwd)"
     popd
     pushd $finalQcowPath
-    & nice "$Global_7z_Compress_Priority" 7z a -t7z "-mmt$Global_7z_Threads" $Global_7z_Compress_Args.Split([char]32) -v249m "$finalArchive" "."
+    & nice "$Global_7z_Compress_Priority" 7z a -t7z "-mmt$Global_7z_Threads" $Global_7z_Compress_Args.Split([char]32) -v49m "$finalArchive" "."
     Say "The Final archive content with compresion ratio [$finalArchive]"
     & 7z l "$($finalArchive).001"
     popd
