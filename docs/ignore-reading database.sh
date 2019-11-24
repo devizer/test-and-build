f0e351df7d96e2e0961695a58a156cf8658c5665
devizer <devizer@users.github.com>
2019-11-24 15:15:55 +0200

    try to clean log during apt install/purge 2, the stable

---
+++ docs/ignore-reading database.sh
@@ -0,0 +1,99 @@
+echo '
+(Reading database ... 10%
+(Reading database ... 15%
+(Reading database ... 20%
+(Reading database ... 25%
+(Reading database ... 30%
+(Reading database ... 35%
+(Reading database ... 40%
+(Reading database ... 45%
+(Reading database ... 50%
+(Reading database ... 55%
+(Reading database ... 60%
+(Reading database ... 65%
+(Reading database ... 70%
+(Reading database ... 75%
+(Reading database ... 80%
+(Reading database ... 85%
+(Reading database ... 90%
+(Reading database ... 95%
+(Reading database ... 100%
+(Reading database ... 29266 files and directories currently installed.)
+Preparing to unpack .../ncurses-bin_6.1+20181013-2+deb10u2_armhf.deb ...
+Unpacking ncurses-bin (6.1+20181013-2+deb10u2) over (6.1+20181013-2+deb10u1) ...
+Setting up ncurses-bin (6.1+20181013-2+deb10u2) ...
+(Reading database ... 
+(Reading database ... 5%
+(Reading database ... 10%
+(Reading database ... 15%
+(Reading database ... 20%
+(Reading database ... 25%
+(Reading database ... 30%
+(Reading database ... 35%
+(Reading database ... 40%
+(Reading database ... 45%
+(Reading database ... 50%
+(Reading database ... 55%
+(Reading database ... 60%
+(Reading database ... 65%
+(Reading database ... 70%
+(Reading database ... 75%
+(Reading database ... 80%
+(Reading database ... 85%
+(Reading database ... 90%
+(Reading database ... 95%
+(Reading database ... 100%
+(Reading database ... 29266 files and directories currently installed.)
+Preparing to unpack .../ncurses-base_6.1+20181013-2+deb10u2_all.deb ...
+Unpacking ncurses-base (6.1+20181013-2+deb10u2) over (6.1+20181013-2+deb10u1) ...
+Setting up ncurses-base (6.1+20181013-2+deb10u2) ...
+(Reading database ... 
+(Reading database ... 5%
+(Reading database ... 10%
+(Reading database ... 15%
+(Reading database ... 20%
+(Reading database ... 25%
+(Reading database ... 30%
+(Reading database ... 35%
+(Reading database ... 40%
+(Reading database ... 45%
+(Reading database ... 50%
+(Reading database ... 55%
+(Reading database ... 60%
+(Reading database ... 65%
+(Reading database ... 70%
+(Reading database ... 75%
+(Reading database ... 80%
+(Reading database ... 85%
+(Reading database ... 90%
+(Reading database ... 95%
+(Reading database ... 100%
+(Reading database ... 29266 files and directories currently installed.)
+Preparing to unpack .../libsystemd0_241-7~deb10u2_armhf.deb ...
+Unpacking libsystemd0:armhf (241-7~deb10u2) over (241-7~deb10u1) ...
+Setting up libsystemd0:armhf (241-7~deb10u2) ...
+(Reading database ... 
+(Reading database ... 5%
+(Reading database ... 10%
+(Reading database ... 15%
+(Reading database ... 20%
+(Reading database ... 25%
+(Reading database ... 30%
+(Reading database ... 35%
+(Reading database ... 40%
+(Reading database ... 45%
+(Reading database ... 50%
+(Reading database ... 55%
+(Reading database ... 60%
+(Reading database ... 65%
+(Reading database ... 70%
+(Reading database ... 75%
+(Reading database ... 80%
+(Reading database ... 85%
+(Reading database ... 90%
+(Reading database ... 95%
+(Reading database ... 100%
+(Reading database ... 29266 files and 
+' | grep -vE 'Reading database.*(%|\.\.\. )$'
+
+
