12a9df42a30cb7c4ee1e8f1861dfba31bdaed942
devizer <devizer@users.github.com>
2019-11-25 20:21:32 +0200

    next qemu-powerman-demo.ps1, ***NO_CI***

--- deploy/Qemu-PowerMan-Demo.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd ~/build/devizer/test-and-build; git pull;
-git pull; pwsh -c ./qemu-powerman-demo.ps1 
-
-
---
+++ deploy/Qemu-PowerMan-Local-Demo.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd ~/build/devizer/test-and-build; 
+git pull; pwsh -c ./qemu-powerman-local-demo.ps1 
+
+
