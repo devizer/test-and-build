9caebb1cbbfa9c292cc9255d5b3da4a3a1f1238f
devizer <devizer@users.github.com>
2019-11-25 05:04:15 +0200

    next qemu-powerman-demo.ps1, ***NO_CI***

--- include/Qemu-PowerMan.ps1
+++ include/Qemu-PowerMan.ps1
@@ -79,8 +79,7 @@ function Qemu-PowerMan-DownloadImage{
     Say "Metadata: [$content_Metadata]"
     $metadata=Qemu-PowerMan-ParseMetadata $content_Metadata
     $metadata | fl
-    Say "STABLE_VERSION: [$($Metadata.STABLE_VERSION)]"
-    Say "DOWNLOAD_PARTS_COUNT: [$($Metadata.DOWNLOAD_PARTS_COUNT)]"
+    Say "'$arch' STABLE_VERSION: [$($Metadata.STABLE_VERSION)], DOWNLOAD_PARTS_COUNT: [$($Metadata.DOWNLOAD_PARTS_COUNT)]"
     $names=@()
     for ($i = 1; $i -le $Metadata.DOWNLOAD_PARTS_COUNT; $i++) {
         $next_url="https://dl.bintray.com/devizer-404/debian-$arch-for-building-and-testing/10.2.604/debian-$arch-final.qcow2.7z.$($i.ToString("000"))";
