939029f534b99edf1509c1494ff4e09d341da39c
devizer <devizer@users.github.com>
2019-11-25 05:19:44 +0200

    next qemu-powerman-demo.ps1, ***NO_CI***

--- include/Qemu-PowerMan.ps1
+++ include/Qemu-PowerMan.ps1
@@ -24,8 +24,8 @@ function Qemu-PowerMan-DownloadBig{
         if (Test-Path $fullName) { Remove-Item $fullName -Force -EA SilentlyContinue }
     }
     $output = (& aria2c "-d$toDirectory" "-Z" $urls 2>&1 ) | Out-String
-    $isOk = $? -or $LASTEXITCODE;
-    Write-Host $output 
+    $isOk = $? -and (-not $LASTEXITCODE);
+    # Write-Host $output 
     if (!$isOk) {
         Write-Error "Error downloading $urls`n$output"
     }
