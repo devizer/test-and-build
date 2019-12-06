2087ff748fb985a5a4c334e28a0a89b71bad35b5
devizer <devizer@users.github.com>
2019-12-06 15:07:24 +0200

    Added Image along with Key again (+Passwords), ***NO_CI***

--- include/Build.ps1
+++ include/Build.ps1
@@ -73,7 +73,7 @@ function Build
     $Global:qemuProcess = [System.Diagnostics.Process]::Start($si)
     $isExited = $Global:qemuProcess.WaitForExit(7000)
 
-    $isOnline = Wait-For-Ssh "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
+    $isOnline = Wait-For-Ssh "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
     if (! $isOnline)
     {
         $Global:IsBuildSuccess=$false;
@@ -90,67 +90,67 @@ function Build
     & ls -la "$mapto"
 
     Say "Copying ./lab/ to guest for [$image::$key]"
-    # Remote-Command-Raw 'mkdir -p /tmp/build' "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
+    # Remote-Command-Raw 'mkdir -p /tmp/build' "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
     & mkdir -p $mapto/tmp/build
     & cp -a $ProjectPath/lab/* $mapto/tmp/build
 
     Say "Configure SSH for [$image::$key]"
-    Remote-Command-Raw "cd /tmp/build; bash config-ssh.sh $key" "localhost" $startParams.Port "root" "$($defenition.UsersPassword)" $true # re-connect
+    Remote-Command-Raw "cd /tmp/build; bash config-ssh.sh $key" "localhost" $startParams.Port "root" "$($definition.UsersPassword)" $true # re-connect
 
     Say "RESTART SSH for [$image::$key]"
-    Remote-Command-Raw 'sshId="$(pgrep -f "sshd -D")"; Say "Restarting SSH Server (pid is $sshId)"; sudo kill -SIGHUP "$(pgrep -f "sshd -D")"; Say "Restarted SSH Server";' "localhost" $startParams.Port "root" "$($defenition.UsersPassword)" $false $true # destructive
+    Remote-Command-Raw 'sshId="$(pgrep -f "sshd -D")"; Say "Restarting SSH Server (pid is $sshId)"; sudo kill -SIGHUP "$(pgrep -f "sshd -D")"; Say "Restarted SSH Server";' "localhost" $startParams.Port "root" "$($definition.UsersPassword)" $false $true # destructive
 
     Say "Configure LC_ALL, UTC and optionally swap for [$image::$key]"
-    Remote-Command-Raw "cd /tmp/build; bash config-system.sh $( $definition.SwapMb )" "localhost" $startParams.Port "root" "$($defenition.UsersPassword)" $true # re-connect
+    Remote-Command-Raw "cd /tmp/build; bash config-system.sh $( $definition.SwapMb )" "localhost" $startParams.Port "root" "$($definition.UsersPassword)" $true # re-connect
 
     if (!$Is_IgnoreAll)
     {
         Say "Upgrading to the latest Debian for [$image::$key]"
-        Remote-Command-Raw 'cd /tmp/build; bash dist-upgrade.sh' "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
+        Remote-Command-Raw 'cd /tmp/build; bash dist-upgrade.sh' "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
 
         Say "Installing locales for [$image::$key]"
-        Remote-Command-Raw 'cd /tmp/build; bash install-locales.sh' "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
+        Remote-Command-Raw 'cd /tmp/build; bash install-locales.sh' "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
     }
 
     # Produce-Report $definition $startParams "onstart"
 
     Say "Greetings from Guest [$image::$key]"
     $cmd = 'Say "Hello from $(whoami). I am the $(hostname) host"; sudo lscpu; echo [PATH] is: $PATH; echo "Content of /etc/default/locale:"; cat /etc/default/locale; echo "[env]"; printenv | sort'
-    Remote-Command-Raw $cmd "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
-    Remote-Command-Raw $cmd "localhost" $startParams.Port "user" "$($defenition.UsersPassword)"
+    Remote-Command-Raw $cmd "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
+    Remote-Command-Raw $cmd "localhost" $startParams.Port "user" "$($definition.UsersPassword)"
 
     if (!$Is_IgnoreAll)
     {
         $mustHavePackages = "smart-apt-install apt-transport-https ca-certificates curl gnupg2 software-properties-common htop mc lsof unzip net-tools bsdutils; apt-get clean"
         Say "Installing must have packages on [$image::$key]"
-        Remote-Command-Raw "$mustHavePackages" "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
+        Remote-Command-Raw "$mustHavePackages" "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
     }
 
     if ($Is_Requested_Dotnet)
     {
         Say "Installing DotNet Core for [$image::$key]"
-        Remote-Command-Raw "cd /tmp/build; bash install-DOTNET.sh; command -v dotnet && dotnet --info || true" "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
-        Remote-Command-Raw 'Say "I am ROOT"; echo PATH is [$PATH]; dotnet --info' "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
-        Remote-Command-Raw 'Say "I am USER"; echo PATH is [$PATH]; dotnet --info' "localhost" $startParams.Port "user" "$($defenition.UsersPassword)"
+        Remote-Command-Raw "cd /tmp/build; bash install-DOTNET.sh; command -v dotnet && dotnet --info || true" "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
+        Remote-Command-Raw 'Say "I am ROOT"; echo PATH is [$PATH]; dotnet --info' "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
+        Remote-Command-Raw 'Say "I am USER"; echo PATH is [$PATH]; dotnet --info' "localhost" $startParams.Port "user" "$($definition.UsersPassword)"
         # TODO: Add dotnet restore
     }
 
     if ($Is_Requested_Mono)
     {
         Say "Installing Latest Mono [$image::$key]"
-        Remote-Command-Raw "cd /tmp/build; bash -e install-MONO.sh" "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
-        Remote-Command-Raw 'Say "I am ROOT"; echo PATH is [$PATH]; mono --version; msbuild /version; echo ""; nuget >/tmp/.tmp; cat /tmp/.tmp | head -4; rm /tmp/.tmp' "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
-        Remote-Command-Raw 'Say "I am USER"; echo PATH is [$PATH]; mono --version; msbuild /version; echo ""; nuget >/tmp/.tmp; cat /tmp/.tmp | head -4; rm /tmp/.tmp' "localhost" $startParams.Port "user" "$($defenition.UsersPassword)"
+        Remote-Command-Raw "cd /tmp/build; bash -e install-MONO.sh" "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
+        Remote-Command-Raw 'Say "I am ROOT"; echo PATH is [$PATH]; mono --version; msbuild /version; echo ""; nuget >/tmp/.tmp; cat /tmp/.tmp | head -4; rm /tmp/.tmp' "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
+        Remote-Command-Raw 'Say "I am USER"; echo PATH is [$PATH]; mono --version; msbuild /version; echo ""; nuget >/tmp/.tmp; cat /tmp/.tmp | head -4; rm /tmp/.tmp' "localhost" $startParams.Port "user" "$($definition.UsersPassword)"
 
         Say "Building NET-TEST-RUNNERS on the host and installing to the guest"
         pushd "$ProjectPath/lab"; & bash NET-TEST-RUNNERS-build.sh; popd
         Say "Copying NET-TEST-RUNNERS to /opt/NET-TEST-RUNNERS on the guest"
         & cp -a ~/build/devizer/NET-TEST-RUNNERS "$mapto/opt"
         Say "Linking NET-TEST-RUNNERS on the guest"
-        Remote-Command-Raw "bash /opt/NET-TEST-RUNNERS/link-unit-test-runners.sh" "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
+        Remote-Command-Raw "bash /opt/NET-TEST-RUNNERS/link-unit-test-runners.sh" "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
 
         Say "Run .net tests on guest [$image::$key]"
-        Remote-Command-Raw "cd /tmp/build; bash -e run-NET-UNIT-TESTS.sh" "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
+        Remote-Command-Raw "cd /tmp/build; bash -e run-NET-UNIT-TESTS.sh" "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
     }
 
 
@@ -158,31 +158,31 @@ function Build
     if ($Is_Requested_Powershell)
     {
         Say "Installing Powershell for [$image::$key]"
-        Remote-Command-Raw "cd /tmp/build; bash -e install-POWERSHELL.sh" "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
+        Remote-Command-Raw "cd /tmp/build; bash -e install-POWERSHELL.sh" "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
     }
 
     if ($Is_Requested_Docker)
     {
         Say "Install Docker for [$image::$key]"
-        Remote-Command-Raw "cd /tmp/build; bash Install-DOCKER.sh;" "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
+        Remote-Command-Raw "cd /tmp/build; bash Install-DOCKER.sh;" "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
     }
 
     if ($Is_Requested_local_postgres)
     {
         Say "Install Local Postgres SQL for [$image::$key]"
-        Remote-Command-Raw "cd /tmp/build; bash install-POSTGRES.sh;" "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
+        Remote-Command-Raw "cd /tmp/build; bash install-POSTGRES.sh;" "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
     }
 
     if ($Is_Requested_Local_Mariadb)
     {
         Say "Install Local MariaDB for [$image::$key]"
-        Remote-Command-Raw "cd /tmp/build; bash install-MARIADB.sh;" "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
+        Remote-Command-Raw "cd /tmp/build; bash install-MARIADB.sh;" "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
     }
 
     if ($Is_Requested_Local_Redis)
     {
         Say "Install Local Redis Server for [$image::$key]"
-        Remote-Command-Raw "cd /tmp/build; bash install-REDIS.sh;" "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
+        Remote-Command-Raw "cd /tmp/build; bash install-REDIS.sh;" "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
     }
 
 
@@ -190,9 +190,9 @@ function Build
     if ($Is_Requested_NodeJS)
     {
         Say "Installing Node for [$image::$key]"
-        Remote-Command-Raw 'cd /tmp/build; export TRAVIS="$TRAVIS"; bash install-NODE.sh' "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
-        Remote-Command-Raw 'Say "As [$(whoami)] NODE: [$(node --version)]; YARN: [$(yarn --version)]; NPM: [$(npm --version)]"; echo PATH is [$PATH];' "localhost" $startParams.Port "user" "$($defenition.UsersPassword)"
-        Remote-Command-Raw 'Say "As [$(whoami)] NODE: [$(node --version)]; YARN: [$(yarn --version)]; NPM: [$(npm --version)]"; echo PATH is [$PATH];' "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
+        Remote-Command-Raw 'cd /tmp/build; export TRAVIS="$TRAVIS"; bash install-NODE.sh' "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
+        Remote-Command-Raw 'Say "As [$(whoami)] NODE: [$(node --version)]; YARN: [$(yarn --version)]; NPM: [$(npm --version)]"; echo PATH is [$PATH];' "localhost" $startParams.Port "user" "$($definition.UsersPassword)"
+        Remote-Command-Raw 'Say "As [$(whoami)] NODE: [$(node --version)]; YARN: [$(yarn --version)]; NPM: [$(npm --version)]"; echo PATH is [$PATH];' "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
     }
     <#
     else {
@@ -204,16 +204,16 @@ function Build
     if (!$Is_IgnoreAll)
     {
         Say "Installing a Crap for [$image::$key]"
-        Remote-Command-Raw "cd /tmp/build; bash install-a-crap.sh" "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
+        Remote-Command-Raw "cd /tmp/build; bash install-a-crap.sh" "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
     }
 
     Say "Log Packages for [$image::$key]"
-    Remote-Command-Raw 'Say "PACKAGES:\n$(list-packages)"' "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
+    Remote-Command-Raw 'Say "PACKAGES:\n$(list-packages)"' "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
 
     Say "Store list-packages"
     $installedPackagesFileName="installed-packages-$key.txt"
     $cmd='list-packages | cut -c 12- | sort > /tmp/' + $installedPackagesFileName
-    Remote-Command-Raw $cmd "localhost" $startParams.Port "root" "$($defenition.UsersPassword)"
+    Remote-Command-Raw $cmd "localhost" $startParams.Port "root" "$($definition.UsersPassword)"
 
     Produce-Report $definition $startParams "onfinish"
 
@@ -233,13 +233,13 @@ function Build
     popd
 
     Say "Zeroing free space of [$image::$key]"
-    Remote-Command-Raw "cd /; bash /tmp/build/TearDown.sh; apt clean; before-compact" "localhost" $startParams.Port "root" "$($defenition.UsersPassword)" $false $true
+    Remote-Command-Raw "cd /; bash /tmp/build/TearDown.sh; apt clean; before-compact" "localhost" $startParams.Port "root" "$($definition.UsersPassword)" $false $true
 
     # Say "Dismounting guest's share of [$key]"
     # & umount -f $mapto # NOOOO shutdown?????
 
     Say "SHUTDOWN [$image::$key] GUEST"
-    Remote-Command-Raw "rm -rf /tmp/build; Say 'Size of the /tmp again:'; du /tmp -d 1 -h; sudo shutdown now" "localhost" $startParams.Port "root" "$($defenition.UsersPassword)" $false $true
+    Remote-Command-Raw "rm -rf /tmp/build; Say 'Size of the /tmp again:'; du /tmp -d 1 -h; sudo shutdown now" "localhost" $startParams.Port "root" "$($definition.UsersPassword)" $false $true
     Wait-For-Process $Global:qemuProcess "$image::$key"
 
     Say "Final compact [$image::$key]"
