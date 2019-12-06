03de0cda782aa50578b8240ea34430693455b6d8
devizer <devizer@users.github.com>
2019-12-06 17:47:15 +0200

    fix BenchmarkDotNet.Tool for CentOS 6, the stable

--- lab/install-DOTNET.sh
+++ lab/install-DOTNET.sh
@@ -12,24 +12,6 @@ if [[ "$ARCH" == "i386" ]]; then
     exit 0; 
 fi
 
-      url=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-dependencies.sh; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -ksSL $url) | bash
-      [[ ! "$(Is-RedHat)" ]] && sudo apt clean
-      DOTNET_Url=https://dot.net/v1/dotnet-install.sh; 
-      mkdir -p ~/.dotnet/tools;
-      sudo mkdir -p /opt/dotnet;
-      export PATH="/opt/dotnet:$HOME/.dotnet/tools:$PATH"
-      export DOTNET_ROOT="/opt/dotnet"
-      mkdir -p /etc/dotnet
-      echo '/opt/dotnet' > /etc/dotnet/install_location
-      # for arm it starts from 2.1
-      try-and-retry curl -o /tmp/_dotnet-install.sh -ksSL $DOTNET_Url
-      time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -c 2.1 -i /opt/dotnet
-      time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -c 2.2 -i /opt/dotnet
-      time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -c 3.0 -i /opt/dotnet
-      try-and-retry dotnet tool install -g BenchmarkDotNet.Tool || true
-      time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -c 3.1 -i /opt/dotnet
-      Say ".NET Core benchmark tool version: [$(dotnet benchmark --version 2>&1)]"
-
 Say "Configuring shared environment for .NET Core"
 echo '#!/usr/bin/env bash
 if [[ -s "/opt/dotnet/dotnet" ]]; then 
@@ -57,5 +39,33 @@ fi
 sudo -u user mkdir -p /home/user/.dotnet/tools
 mkdir -p ~/.dotnet/tools
 Say "Configured shared environment for .NET Core"
+. /etc/profile.d/dotnet-core.sh
+
+
+      export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+      export DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0
+      url=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-dependencies.sh; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -ksSL $url) | bash
+      [[ ! "$(Is-RedHat)" ]] && sudo apt clean
+      DOTNET_Url=https://dot.net/v1/dotnet-install.sh; 
+      mkdir -p ~/.dotnet/tools;
+      sudo mkdir -p /opt/dotnet;
+      export PATH="/opt/dotnet:$HOME/.dotnet/tools:$PATH"
+      export DOTNET_ROOT="/opt/dotnet"
+      mkdir -p /etc/dotnet
+      echo '/opt/dotnet' > /etc/dotnet/install_location
+      # for arm it starts from 2.1
+      try-and-retry curl -o /tmp/_dotnet-install.sh -ksSL $DOTNET_Url
+      Say "Installing .NET Core 2.1 SDK"
+      time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -c 2.1 -i /opt/dotnet
+      Say "Installing .NET Core 2.2 SDK"
+      time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -c 2.2 -i /opt/dotnet
+      Say "Installing .NET Core 3.0 SDK"
+      time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -c 3.0 -i /opt/dotnet
+      Say "Installing BenchmarkDotNet.Tool (globally)"
+      try-and-retry dotnet tool install -g BenchmarkDotNet.Tool || true
+      Say "Installing .NET Core 3.1 SDK"
+      time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -c 3.1 -i /opt/dotnet
+      Say ".NET Core benchmark tool version: [$(dotnet benchmark --version 2>&1)]"
+
 
 # todo: BenchmarkDotNet.Tool for root
