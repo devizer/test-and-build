fcb35548def090127b9b0e1da041dfa491f7c50e
devizer <devizer@users.github.com>
2019-12-10 07:46:56 +0200

    added accelerated sdb1 benchmark2, the stable

--- docs/how-to-accelerate-sdb1.sh
+++ docs/how-to-accelerate-sdb1.sh
@@ -3,20 +3,5 @@ path="/sdb1-accelerated"
 sudo mkdir -p "$path"
 sudo umount /mnt
 mkfs.ext2 -f -L ext2-accelerated "$file"
-sudo mount -t ext2 "$file" "$path" -o defaults,noatime,nodiratime,
+sudo mount -t ext2 "$file" "$path" -o rw,noatime,nodiratime,errors=remount-ro
 
-
-if [ true ]; then
-    
-    
-else
-    file="/mnt/BTRFS.disk"
-    sudo dd if=/dev/zero of="/$file" bs=1 seek=12800M count=1
-fi
-
-sudo mkfs.btrfs -f -L a-disk "$file" -O ^extref,^skinny-metadata
-echo "MOUNTING $file as $path"
-sudo mount -t btrfs "$file" "$path" -o defaults,noatime,nodiratime,compress-force
-sudo chown $(whoami) $path
-touch $path/hi
-df -T -h
