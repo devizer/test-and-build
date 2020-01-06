e627d0e07f96532261b0eb2438a7ce2a5db92b49
devizer <devizer@users.github.com>
2020-01-06 03:58:25 +0200

    large fix of local demo, the stable

--- qemu-powerman-local-demo.ps1
+++ qemu-powerman-local-demo.ps1
@@ -7,8 +7,8 @@ if (Test-Path "V:\" -PathType Container)
     $Global:Qemu_PowerMan_DownloadImageLocation = "V:\Qemu_PowerMan_DownloadImageLocation"
 }
 
-# $ok1 = Qemu-PowerMan-DownloadImage "CentOS-6-AMD64"
+$ok1 = Qemu-PowerMan-DownloadImage "CentOS-6-AMD64"
 $ok2 = Qemu-PowerMan-DownloadImage "Debian-10-arm64"
-# $ok3 = Qemu-PowerMan-DownloadImage "Debian-10-arm"
-# $ok3 = Qemu-PowerMan-DownloadImage "Debian-10-AMD64"
+$ok3 = Qemu-PowerMan-DownloadImage "Debian-10-arm"
+$ok4 = Qemu-PowerMan-DownloadImage "Debian-10-AMD64"
 
