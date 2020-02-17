af97113a7e0b5b4a93d27112c630e9b1f6d59ead
devizer <devizer@github.io>
2020-02-17 08:41:12 +0200

    DEBIAN_FRONTEND=noninteractive for dist-upgrade

--- lab/dist-upgrade.sh
+++ lab/dist-upgrade.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 lazy-apt-update
 Say "Upgrading to the latest Debian" 
-sudo apt dist-upgrade
+export DEBIAN_FRONTEND=noninteractive 
+sudo DEBIAN_FRONTEND=noninteractive apt dist-upgrade
 sudo apt clean
 
 Say "Content of the /boot folder"
