6d8b49b49aae8471077521f0c2e86f4f3f43997a
devizer <devizer@users.github.io>
2020-05-20 23:14:58 +0300

    Show-System-Stat.sh: added disk stat

--- lab/permanent-scripts/Show-System-Stat.sh
+++ lab/permanent-scripts/Show-System-Stat.sh
@@ -29,6 +29,12 @@ function GetUptimeInSeconds() {
    echo $uptime
 }
 
+function format_label() {
+    local label="$1"
+    label="${label} ";local n=0; while [[ $n -lt 55 && "${#label}" -lt 38 ]]; do n=$((n+1));label="${label}."; done
+    echo "$label"
+}
+
 function ShowSystemStat() {
   if [[ "$(uname_system)" == Darwin || ! -f /proc/stat ]]; then
     uptime
@@ -56,17 +62,17 @@ function ShowSystemStat() {
   echo "User CPU Usage (normal priority) ..... $user_normal_formatted
 User CPU Usage (low priority) ........ $user_nice_formatted
 System CPU Usage ..................... $system_formatted
---------------------------------------
-Total CPU Usage ...................... $total_formatted
+TOTAL CPU Usage ...................... $total_formatted
 Uptime ............................... $uptime_formatted"
 }
 
 function FormatBytes() {
     local bytes=$1
-    if [[ "$bytes" -lt 9000 ]]; then bytes="$bytes bytes"; 
-    elif [[ "$bytes" -lt 9000000 ]]; then bytes=$((bytes/1024)); bytes="$bytes Kb";
-    elif [[ "$bytes" -lt 9000000000 ]]; then bytes=$((bytes/1048576)); bytes="$bytes Mb";
-    else bytes=$((bytes/1073741824)); bytes="$bytes Gb";
+    if [[ "$bytes" -lt 9000 ]]; then bytes="$bytes B"; 
+    elif [[ "$bytes" -lt 9000000 ]]; then bytes=$((bytes/1024)); bytes="$bytes KB";
+    elif [[ "$bytes" -lt 9000000000 ]]; then bytes=$((bytes/1048576)); bytes="$bytes MB";
+    elif [[ "$bytes" -lt 9000000000000 ]]; then bytes=$((bytes/1073741824)); bytes="$bytes GB";
+    else bytes=$((bytes/1099511627776)); bytes="$bytes TB";
     fi
     echo $bytes
 }
@@ -80,7 +86,7 @@ function ShowNetStat() {
         if [[ "$name" == *":" ]]; then
             local recv=$(echo $line | awk '{print $2}')
             local sent=$(echo $line | awk '{print $10}')
-            name="${name} ";local n=0; while [[ $n -lt 55 && "${#name}" -lt 38 ]]; do n=$((n+1));name="${name}."; done
+            name=$(format_label "$name");
             if [[ "$sent" -gt 0 && "$recv" -gt 0 ]]; then
                 sent=$(FormatBytes $sent)
                 recv=$(FormatBytes $recv)
@@ -89,10 +95,55 @@ function ShowNetStat() {
                 echo "$name ${sent_formatted} [sent] + ${recv_formatted} [recieved]"
             fi  
         fi 
-        
     done
+}
+
+function parseStat() {
+    # args: statDir, statTitle
+    local size="$(cat $statDir/size 2>/dev/null)"
+    if [[ "$size" != "0" && "$size" != "" && "$size" -gt 0 ]]; then
+        local line="$(cat $statDir/stat)";
+        IO_MILLISECONDS=$(echo $line | awk '{print $10}');
+        READ_SECTORS=$(echo $line | awk '{print $3}');
+        WRITE_SECTORS=$(echo $line | awk '{print $7}');
+        if [[ $IO_MILLISECONDS -gt 0 && $READ_SECTORS -gt 0 && $WRITE_SECTORS -gt 0 ]]; then
+            local read=$(FormatBytes $((READ_SECTORS*512)))
+            local wrote=$(FormatBytes $((WRITE_SECTORS*512)))
+            local read_formatted=`printf %-7s "$read"`
+            local wrote_formatted=`printf %-7s "$wrote"`
+            local io_time_formatted=$(format_seconds $((IO_MILLISECONDS/1000)))
+            local title_formatted=$(format_label "$statTitle")
+            echo "$title_formatted ${wrote_formatted} [wrote] + ${read_formatted} [read], ${io_time_formatted} io"
+        fi 
+    fi
+}
+
+function ShowBlockStat() {
+    blockDirs=$(ls -1 /sys/block 2>/dev/null);
+    for blockDirName in $blockDirs; do
+        local blockDir=/sys/block/$blockDirName
+        if [[ -d $blockDir ]]; then
+            statDir="${blockDir}";
+            statTitle="/dev/$(basename $blockDir)";
+            parseStat
+            volDirs=$(ls -1 $blockDir 2>/dev/null);
+            for volDirName in $volDirs; do
+                local volDir="$blockDir/$volDirName"
+                # echo "VOL: $volDir" 
+                if [[ -d $volDir ]]; then
+                    statDir="${volDir}";
+                    statTitle=" --- $(basename $volDirName)";
+                    parseStat
+                fi 
+            done 
+        fi
+    done;
+    
 }
 
 # while true; do clear; ShowSystemStat; sleep 2; done
 ShowSystemStat
+echo "------------NETWORK-------------------"
 ShowNetStat
+echo "------------DISKS---------------------"
+ShowBlockStat
