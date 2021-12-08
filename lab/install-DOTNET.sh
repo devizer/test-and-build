e969ce94ad464d8fa5cd9853c4390bcdb9c5d4c0
devizer <devizer@github.users>
2021-12-08 03:40:40 +0200

    added net core any version sdk ***no_ci***

--- lab/install-DOTNET.sh
+++ lab/install-DOTNET.sh
@@ -86,7 +86,9 @@ Say "Configured shared environment for .NET Core"
 
       export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
       # export DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0
-      url=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-dependencies.sh; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -ksSL $url) | bash
+      if [[ "${SKIP_DOTNET_DEPENDENCIES:-}" != "True" ]]; then
+        url=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-dependencies.sh; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -ksSL $url) | bash
+      fi
       [[ ! "$(Is-RedHat)" ]] && sudo apt-get clean
       DOTNET_Url=https://dot.net/v1/dotnet-install.sh; 
       mkdir -p ~/.dotnet/tools;
@@ -99,75 +101,17 @@ Say "Configured shared environment for .NET Core"
       try-and-retry curl -o /tmp/_dotnet-install.sh -ksSL $DOTNET_Url
       export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
       
-      if false && [[ "$(uname -m)" == "x86_64" ]]; then
-          # rzc fails if .NET Core SDK 2.0 installed
-          Say "Installing .NET Core 1.0 SDK for $(uname -m)"
-          time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -c 1.0 -i ${DOTNET_TARGET_DIR}
-          Say "Installing .NET Core 1.1 SDK for $(uname -m)"
-          time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -c 1.1 -i ${DOTNET_TARGET_DIR}
-          Say "Installing .NET Core 2.0 SDK for $(uname -m)"
-          time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -c 2.0 -i ${DOTNET_TARGET_DIR}
-      fi
+      for v in $DOTNET_VERSIONS; do
+        pat='^[0-9]+\.[0-9]+$'
+        if [[ $v =~ $pat ]]; then 
+          __a="-c $v"
+          __m="$v (channel)"
+        else 
+          __a="-version $v"
+          __m="$v (version)"
+        fi
+        __machine="${__machine:-$(uname -m)}"
+        Say "Installing .NET Core $__m SDK for $__machine"
+        time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh $__a -i ${DOTNET_TARGET_DIR}
+      done
       
-      
-      if [[ "$DOTNET_VERSIONS2" == *" 2.1 "* ]]; then
-          Say "Installing .NET Core 2.1 SDK for $(uname -m)"
-          time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -c 2.1 -i ${DOTNET_TARGET_DIR}
-      fi
-      
-      if [[ "$DOTNET_VERSIONS2" == *" 2.2 "* ]]; then
-        sleep 6
-        Say "Installing .NET Core 2.2 SDK for $(uname -m)"
-        time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -c 2.2 -i ${DOTNET_TARGET_DIR}
-      fi
-
-      
-      # if [[ "$(command -v dotnet || true)" == "" ]]; then
-      #     BenchmarkDotNet_Installed=false        
-      # elif [[ "$(command -v dotnet-benchmark || true)" == "" ]]; then
-      #     Say "Installing BenchmarkDotNet.Tool (globally) for $(uname -m)"
-      #     Say "DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER is '${DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER}'"
-      #     try-and-retry dotnet tool install -g BenchmarkDotNet.Tool || true
-      #     BenchmarkDotNet_Installed=true
-      # else
-      #     Say "BenchmarkDotNet.Tool already installed"
-      #     BenchmarkDotNet_Installed=true
-      # fi
-
-      if [[ "$DOTNET_VERSIONS2" == *" 3.0 "* ]]; then
-        Say "Installing .NET Core 3.0 SDK for $(uname -m)"
-        time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -c 3.0 -i ${DOTNET_TARGET_DIR}
-      fi
-      
-      if [[ "$DOTNET_VERSIONS2" == *" 3.1 "* ]]; then
-        Say "Installing .NET Core 3.1 SDK for $(uname -m)"
-        time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -c 3.1 -i ${DOTNET_TARGET_DIR}
-      fi
-      
-      if [[ "$DOTNET_VERSIONS2" == *" 3.1.120 "* ]]; then
-        Say "Installing .NET Core 3.1.120 SDK for $(uname -m)"
-        time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -version 3.1.120 -i ${DOTNET_TARGET_DIR}
-      fi
-      
-      if [[ "$DOTNET_VERSIONS2" == *" 5.0 "* ]]; then
-        Say "Installing .NET Core 5.0 SDK for $(uname -m)"
-        time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -c 5.0 -i ${DOTNET_TARGET_DIR}
-      fi
-      
-      if [[ "$DOTNET_VERSIONS2" == *" 6.0 "* ]]; then
-        Say "Installing .NET Core 6.0 SDK for $(uname -m)"
-        time try-and-retry timeout 666 sudo -E bash /tmp/_dotnet-install.sh -c 6.0 -i ${DOTNET_TARGET_DIR}
-      fi
-      
-      # if [[ "$BenchmarkDotNet_Installed" == "false" ]]; then
-      #     Say "Installing BenchmarkDotNet.Tool (globally) for $(uname -m)"
-      #     try-and-retry dotnet tool install -g BenchmarkDotNet.Tool || true
-      # fi
-      
-      # ! { Say ".NET Core benchmark tool version: [$(dotnet benchmark --version 2>&1 || true)]" }
-      # dotnet benchmark --version >/dev/null 2>&1 || true;
-      # Say ".NET Core benchmark tool version: [$(dotnet benchmark --version 2>/dev/null || true)]"
-      true
-
-
-# todo: BenchmarkDotNet.Tool for root
