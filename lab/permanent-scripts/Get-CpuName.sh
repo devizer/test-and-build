c62e843aeedde31473a602dd481a18037a03667b
devizer <devizer@users.github.io>
2023-02-27 20:17:37 +0200

    Get-CpuName.sh --name-only ***NO_CI***

--- lab/permanent-scripts/Get-CpuName.sh
+++ lab/permanent-scripts/Get-CpuName.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
+if [[ "$1" == "--name-only" ]]; then nameOnly="true"; fi
 function get_cpu_name() {
   if [[ "$(uname -s)" == Linux ]]; then
     cpu="$(cat /proc/cpuinfo | grep -E '^(model name|Hardware)' | awk -F':' 'NR==1 {print $2}')"; 
     cpu="$(echo -e "${cpu:-}" | sed -e 's/^[[:space:]]*//')"
-    if [[ -n "(command -v nproc)" ]]; then cpu="$cpu, $(nproc) Cores"; fi
+    if [[ -z "$nameOnly" ]] && [[ -n "(command -v nproc)" ]]; then cpu="$cpu, $(nproc) Cores"; fi
     # todo: cat /proc/device-tree/model on raspberry
     echo "${cpu}"
   elif [[ "$(uname -s)" == Darwin ]]; then
-    cpu="$(sysctl -n machdep.cpu.brand_string), $(sysctl -n machdep.cpu.core_count) Cores, $(sysctl -n machdep.cpu.thread_count) Threads"
+    cpu="$(sysctl -n machdep.cpu.brand_string)"
+    if [[ -z "$nameOnly" ]]; then
+      cpu="$cpu, $(sysctl -n machdep.cpu.core_count) Cores, $(sysctl -n machdep.cpu.thread_count) Threads"
+    fi
     echo "${cpu}"
   elif [[ "$(uname -s)" == *"MINGW"* ]]; then
-    cpu="$(echo 'Write-Host "$((Get-WmiObject Win32_Processor).Name), $([Environment]::ProcessorCount) Cores"' | powershell -c -)"
+    if [[ -z "$nameOnly" ]]; then
+      cpu="$(echo 'Write-Host "$((Get-WmiObject Win32_Processor).Name), $([Environment]::ProcessorCount) Cores"' | powershell -c -)"
+    else
+      cpu="$(echo 'Write-Host "$((Get-WmiObject Win32_Processor).Name)"' | powershell -c -)"
+    fi
     echo "$cpu"
   else
     cpu="Unknown '$(uname -m)' cpu name"
-    if [[ -n "(command -v nproc)" ]]; then cpu="$cpu, $(nproc) Cores"; fi
+    if [[ -z "$nameOnly" ]] && [[ -n "(command -v nproc)" ]]; then cpu="$cpu, $(nproc) Cores"; fi
   fi
 }
 
